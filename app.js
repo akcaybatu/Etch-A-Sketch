1b362df74bf8a1a88f9307876f05609240157878
Batuhan <akcay.batu76@gmail.com>
2021-07-27 12:49:36 +0300

    Project still in progress

--- app.js
+++ app.js
@@ -5,43 +5,35 @@ let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
 window.addEventListener("load", defaultGrid);
-// Creates a default grid sized 16x16
+
 function defaultGrid() {
     makeRows(16);
     makeColumns(16);
+    let myCell = document.getElementsByClassName("cell");
+    for (let index = 0; index < myCell.length; index++) {
+        const element = myCell[index];
+        element.addEventListener('mousemove', runEvent);    
+    }
+    
 }
 
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
+function runEvent(e){
+    console.log("Hello MF");
+    e.currentTarget.style.backgroundColor = "rgb(255, 7, 3)";
+}
 
-    // Creates rows
+function makeRows(rowNum) {
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
-    };
-};
+    }
+}
 
-// Creates columns
 function makeColumns(cellNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
-
-// defaultGrid();
-
-// document.getElementsByClassName("lala").addEventListener('click', runEvent);
-
-// function runEvent(e){
-//     console.log("Hello MF");
-//     document.getElementsByClassName("lala").style.backgroundColor = "rgb(255, 7, 3)";
-// }
-
-function changeColor(e){
-    console.log("Hello")
-    e.target.style.backgroundColor = "rgb(200, 4, 2)";
+        }
+    }
 }
